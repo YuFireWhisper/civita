71b2d81442c53cc8612e19e1a073b7a5be9ab966
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-02-26 15:27:55 +0800

    feat: Implement Vrf::verify

--- src/core/vrf.rs
+++ src/core/vrf.rs
@@ -1,4 +1,4 @@
-use libp2p::identity::Keypair;
+use libp2p::identity::{Keypair, PublicKey};
 use sha2::{Digest, Sha256};
 use thiserror::Error;
 
@@ -37,6 +37,27 @@ impl Vrf {
         hasher.update(input);
         hasher.finalize().to_vec()
     }
+
+    pub fn verify(
+        public_key: &PublicKey,
+        input: &[u8],
+        output: &[u8],
+        proof: &[u8],
+    ) -> VrfResult<bool> {
+        let is_signature_valid = public_key.verify(input, proof);
+        if !is_signature_valid {
+            return Ok(false);
+        }
+
+        let expected_output = Self::compute_expected_output(proof);
+        Ok(expected_output == output)
+    }
+
+    fn compute_expected_output(proof: &[u8]) -> Vec<u8> {
+        let mut hasher = Sha256::new();
+        hasher.update(proof);
+        hasher.finalize().to_vec()
+    }
 }
 
 #[cfg(test)]
@@ -68,5 +89,31 @@ mod tests {
 
         assert_eq!(output.len(), 32, "Vrf output should be 32 bytes long");
         assert_eq!(signature.len(), 64, "Vrf signature should be 64 bytes long");
+    }
+
+    #[test]
+    fn test_verify_valid() {
+        let keypair = Keypair::generate_ed25519();
+        let vrf = Vrf::new(keypair.clone());
+
+        let input = b"input";
+        let (output, proof) = vrf.compute(input).unwrap();
+
+        let is_valid = Vrf::verify(&keypair.public(), input, &output, &proof).unwrap();
+
+        assert!(is_valid, "Vrf should verify the output and proof");
+    }
+
+    #[test]
+    fn test_verify_invalid() {
+        let keypair = Keypair::generate_ed25519();
+        let vrf = Vrf::new(keypair.clone());
+
+        let input = b"input";
+        let (output, _) = vrf.compute(input).unwrap();
+
+        let is_valid = Vrf::verify(&keypair.public(), input, &output, &[0; 64]).unwrap();
+
+        assert!(!is_valid, "Vrf should not verify the output and proof");
     }
 }
