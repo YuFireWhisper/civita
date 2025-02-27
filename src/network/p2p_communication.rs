35b2342233c5059754c36d8f67f55f023ca61d25
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-02-27 11:27:55 +0800

    feat: Add Error enum to P2PCommunication

--- src/network/p2p_communication.rs
+++ src/network/p2p_communication.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use libp2p::{
     core::{muxing::StreamMuxerBox, transport::Boxed, upgrade::Version},
     identity::Keypair,
@@ -6,15 +8,24 @@ use libp2p::{
     tcp::tokio,
     yamux, Multiaddr, PeerId, Swarm, Transport,
 };
+use thiserror::Error;
 
 use super::p2p_behaviour::P2PBehaviour;
+
+#[derive(Debug, Error)]
+pub enum P2PCommunicationError {
+    #[error("Swarm Transport Error: {0}")]
+    SwarmTransport(#[from] libp2p::TransportError<io::Error>),
+}
+
+type P2PCommunicationResult<T> = Result<T, P2PCommunicationError>;
 
 pub struct P2PCommunication {
     swarm: Swarm<P2PBehaviour>,
 }
 
 impl P2PCommunication {
-    pub fn new(keypair: Keypair, listen_addr: Multiaddr) -> Self {
+    pub fn new(keypair: Keypair, listen_addr: Multiaddr) -> P2PCommunicationResult<Self> {
         let transport = Self::create_transport(keypair.clone());
         let behaviour = P2PBehaviour::new(keypair.clone());
 
@@ -24,9 +35,9 @@ impl P2PCommunication {
             PeerId::from_public_key(&keypair.public()),
             swarm::Config::with_tokio_executor(),
         );
-        swarm.listen_on(listen_addr).unwrap();
+        swarm.listen_on(listen_addr)?;
 
-        Self { swarm }
+        Ok(Self { swarm })
     }
 
     fn create_transport(keypair: Keypair) -> Boxed<(PeerId, StreamMuxerBox)> {
@@ -40,11 +51,11 @@ impl P2PCommunication {
 
 #[cfg(test)]
 mod tests {
+    use libp2p::{futures::StreamExt, identity::Keypair, swarm::SwarmEvent, Multiaddr, Swarm};
     use std::time::Duration;
-
-    use crate::network::p2p_communication::P2PCommunication;
-    use libp2p::{futures::StreamExt, identity::Keypair, swarm::SwarmEvent, Multiaddr};
     use tokio::time::timeout;
+
+    use crate::network::{p2p_behaviour::P2PBehaviour, p2p_communication::P2PCommunication};
 
     const TIMEOUT_DURATION: Duration = Duration::from_secs(5);
 
@@ -53,36 +64,37 @@ mod tests {
     }
 
     impl TestCommunication {
-        async fn new() -> Result<Self, &'static str> {
+        async fn new() -> Result<Self, Box<dyn std::error::Error>> {
             let keypair = Keypair::generate_ed25519();
-            let listen_addr: Multiaddr = "/ip4/0.0.0.0/tcp/0".parse().unwrap();
+            let listen_addr: Multiaddr = "/ip4/0.0.0.0/tcp/0".parse()?;
 
-            let mut p2p = P2PCommunication::new(keypair, listen_addr);
+            let mut p2p = P2PCommunication::new(keypair, listen_addr)?;
+            Self::wait_for_listen_addr(&mut p2p.swarm).await?;
 
-            let is_ready = timeout(TIMEOUT_DURATION, async {
-                while let Some(event) = p2p.swarm.next().await {
+            Ok(Self { p2p })
+        }
+
+        async fn wait_for_listen_addr(swarm: &mut Swarm<P2PBehaviour>) -> Result<(), &'static str> {
+            timeout(TIMEOUT_DURATION, async {
+                while let Some(event) = swarm.next().await {
                     if let SwarmEvent::NewListenAddr { .. } = event {
-                        return true;
+                        return Ok(());
                     }
                 }
-                false
+                Err("Timeout waiting for listen address")
             })
             .await
-            .map_err(|_| "Failed to create P2PCommunication")?;
-
-            if is_ready {
-                Ok(Self { p2p })
-            } else {
-                Err("Failed to create P2PCommunication")
-            }
+            .map_err(|_| "Timeout waiting for listen address")?
         }
     }
 
     #[tokio::test]
-    async fn test_new() {
+    async fn test_p2p_communication_creation() {
+        let result = TestCommunication::new().await;
         assert!(
-            TestCommunication::new().await.is_ok(),
-            "Failed to create P2PCommunication"
+            result.is_ok(),
+            "Failed to create P2PCommunication: {:?}",
+            result.err()
         );
     }
 }
