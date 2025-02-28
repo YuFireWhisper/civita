63ae091bf529cb788477834d8cb4d0648fd33103
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-02-28 20:11:36 +0800

    feat: Implement ConnectionManager::add_peer

--- src/network/connection_manager.rs
+++ src/network/connection_manager.rs
@@ -21,6 +21,27 @@ impl ConnectionManager {
             connection_timeout,
         }
     }
+
+    pub fn add_peer(&mut self, peer_id: PeerId, addr: Multiaddr) {
+        if self.connections.contains_key(&peer_id) {
+            return;
+        }
+
+        let connection = self
+            .connections
+            .entry(peer_id)
+            .or_insert_with(|| Connection {
+                peer_id,
+                addresses: Vec::new(),
+                connected_point: None,
+                last_seen: Instant::now(),
+                status: ConnectionStatus::Disconnected,
+            });
+
+        if !connection.addresses.contains(&addr) {
+            connection.addresses.push(addr);
+        }
+    }
 }
 
 pub struct Connection {
@@ -40,9 +61,11 @@ pub enum ConnectionStatus {
 
 #[cfg(test)]
 mod tests {
+    use crate::network::connection_manager::ConnectionManager;
+    use libp2p::{Multiaddr, PeerId};
     use std::time::Duration;
 
-    use crate::network::connection_manager::ConnectionManager;
+    const PEER_ADDR: &str = "/ip4/0.0.0.0/tcp/0";
 
     #[test]
     fn test_new() {
@@ -54,5 +77,20 @@ mod tests {
         assert_eq!(connection_manager.bootstrap_peers.len(), 0);
         assert_eq!(connection_manager.banned_peers.len(), 0);
         assert_eq!(connection_manager.connection_timeout, connection_timeout);
+    }
+
+    #[test]
+    fn test_add_peer() {
+        let bootstrap_peers = vec![];
+        let connection_timeout = Duration::from_secs(10);
+        let mut connection_manager = ConnectionManager::new(bootstrap_peers, connection_timeout);
+
+        let peer_id = PeerId::random();
+        let addr: Multiaddr = PEER_ADDR.parse().unwrap();
+        connection_manager.add_peer(peer_id, addr.clone());
+
+        assert_eq!(connection_manager.connections.len(), 1);
+        assert_eq!(connection_manager.connections[&peer_id].addresses.len(), 1);
+        assert_eq!(connection_manager.connections[&peer_id].addresses[0], addr);
     }
 }
