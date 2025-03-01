c260b4e2f451f8109e64e9719a3ee6b07690ed55
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-01 10:59:39 +0800

    feat: Implement DiscoveryService::add_peer_address

--- src/network/discovery_service.rs
+++ src/network/discovery_service.rs
@@ -18,17 +18,50 @@ impl DiscoveryService {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn add_peer_address(&mut self, peer_id: PeerId, addr: Multiaddr) {
+        let addresses = self.peer_addresses.entry(peer_id).or_default();
+        if !addresses.contains(&addr) {
+            addresses.push(addr);
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const PEER_ADDR: &str = "/ip4/0.0.0.0/tcp/0";
+
     #[test]
     fn test_new() {
         let discovery_service = DiscoveryService::new();
+
         assert_eq!(discovery_service.kad_queries.len(), 0);
         assert_eq!(discovery_service.peer_addresses.len(), 0);
         assert_eq!(discovery_service.pending_find_peer_requests.len(), 0);
+    }
+
+    #[test]
+    fn test_add_peer_address() {
+        let mut discovery_service = DiscoveryService::new();
+        let peer_id = PeerId::random();
+        let addr: Multiaddr = PEER_ADDR.parse().unwrap();
+
+        discovery_service.add_peer_address(peer_id, addr.clone());
+
+        assert_eq!(discovery_service.peer_addresses.len(), 1);
+        assert_eq!(
+            discovery_service
+                .peer_addresses
+                .get(&peer_id)
+                .unwrap()
+                .len(),
+            1
+        );
+        assert_eq!(
+            discovery_service.peer_addresses.get(&peer_id).unwrap()[0],
+            addr
+        );
     }
 }
