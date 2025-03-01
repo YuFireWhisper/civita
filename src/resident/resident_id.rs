7ba01f2dda6e12f686c4384fa0575c11f4f53a5d
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-01 14:41:13 +0800

    feat: Implement ResidentId::random

--- src/resident/resident_id.rs
+++ src/resident/resident_id.rs
@@ -1,15 +1,22 @@
 use libp2p::PeerId;
 
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct ResidentId(pub PeerId);
 
 impl ResidentId {
     pub fn new(peer_id: PeerId) -> Self {
         Self(peer_id)
     }
+
+    pub fn random() -> Self {
+        Self(PeerId::random())
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use libp2p::PeerId;
 
     use crate::resident::resident_id::ResidentId;
@@ -19,5 +26,21 @@ mod tests {
         let peer_id = PeerId::random();
         let resident_id = ResidentId::new(peer_id);
         assert_eq!(resident_id.0, peer_id, "ResidentId should store the PeerId");
+    }
+
+    #[test]
+    fn test_random() {
+        const NUM_IDS: usize = 10;
+
+        let mut resident_ids = HashSet::new();
+        for _ in 0..NUM_IDS {
+            resident_ids.insert(ResidentId::random());
+        }
+
+        assert_eq!(
+            resident_ids.len(),
+            NUM_IDS,
+            "ResidentId::random() should generate unique IDs"
+        );
     }
 }
