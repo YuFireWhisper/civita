71e893f7511f5cde0a5eb4107835f37274879ead
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-02 14:43:39 +0800

    style: Formatting P2PCommunication

--- src/network/p2p_communication.rs
+++ src/network/p2p_communication.rs
@@ -373,7 +373,9 @@ mod tests {
 
         node.p2p.is_receiving_messages.store(true, Ordering::SeqCst);
 
-        node.p2p.stop_receive().expect("Failed to stop receiving messages");
+        node.p2p
+            .stop_receive()
+            .expect("Failed to stop receiving messages");
 
         assert!(
             !node.p2p.is_receiving_messages.load(Ordering::SeqCst),
