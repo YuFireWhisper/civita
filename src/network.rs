8ed5debdc89dece2e6acb43e8a03ac6c775a50f7
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-03 18:33:45 +0800

    refactor: Remove MessageType

--- src/network.rs
+++ src/network.rs
@@ -1,7 +1,6 @@
+pub mod behaviour;
 pub mod connection_manager;
 pub mod heartbeat_service;
 pub mod message;
-pub mod message_type;
 pub mod resident_network;
 pub mod transport;
-pub mod behaviour;
