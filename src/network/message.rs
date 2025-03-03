1174924267ce6f2c4c5a2935b79376991c8a4ba8
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-03 10:14:59 +0800

    refactor: Change Message struct and implement Message::new

--- src/network/message.rs
+++ src/network/message.rs
@@ -1,14 +1,55 @@
+use libp2p::{gossipsub::MessageId, identity::Keypair, PeerId};
 use serde::{Deserialize, Serialize};
 
-use super::message_type::MessageType;
+use crate::crypto::signature::Signature;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Message {
-    pub message_type: MessageType,
-    pub source: String,
-    pub target: Option<String>,
-    pub community_id: Option<String>,
-    pub payload: Vec<u8>,
-    pub signature: Vec<u8>,
+    pub message_id: Option<MessageId>,
+    pub source: PeerId,
+    pub topic: String,
+    pub content: Vec<u8>,
     pub timestamp: u64,
+    pub signature: Option<Vec<u8>>,
+}
+
+impl Message {
+    pub fn new(keypair: Keypair, topic: &str, content: Vec<u8>) -> Result<Self, String> {
+        let source = PeerId::from_public_key(&keypair.public());
+        let timestamp = chrono::Utc::now().timestamp() as u64;
+
+        let mut message = Self {
+            message_id: None,
+            source,
+            topic: topic.to_string(),
+            content,
+            timestamp,
+            signature: None,
+        };
+
+        let signature = Signature::new(keypair).sign(&serde_json::to_vec(&message).unwrap())?;
+        message.signature = Some(signature);
+
+        Ok(message)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use libp2p::{identity::Keypair, PeerId};
+
+    use crate::network::transport::test_communication::TEST_TOPIC;
+
+    use super::Message;
+
+    const TEST_CONTENT: &[u8] = b"content";
+
+    #[test]
+    fn test_new() {
+        let keypair = Keypair::generate_ed25519();
+
+        let message = Message::new(keypair.clone(), TEST_TOPIC, TEST_CONTENT.to_vec()).unwrap();
+
+        assert_eq!(message.source, PeerId::from_public_key(&keypair.public()));
+    }
 }
