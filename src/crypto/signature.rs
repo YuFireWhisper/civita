b55716a359df55c6efa25e90625d0d758e8a4d0b
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-03 09:54:49 +0800

    feat: Implement Signature::sign

--- src/crypto/signature.rs
+++ src/crypto/signature.rs
@@ -8,6 +8,13 @@ impl Signature {
     pub fn new(keypair: Keypair) -> Self {
         Self { keypair }
     }
+
+    pub fn sign(&self, input: &[u8]) -> Result<Vec<u8>, String> {
+        self.keypair
+            .sign(input)
+            .map(|sig| sig.to_vec())
+            .map_err(|e| e.to_string())
+    }
 }
 
 #[cfg(test)]
@@ -26,6 +33,20 @@ mod tests {
             signature.keypair.public(),
             keypair.public(),
             "Signature should store the keypair"
+        );
+    }
+
+    #[test]
+    fn test_sign() {
+        let keypair = Keypair::generate_ed25519();
+        let signature = Signature::new(keypair.clone());
+
+        let input = b"input";
+        let signature_bytes = signature.sign(input).unwrap();
+
+        assert!(
+            keypair.public().verify(input, &signature_bytes),
+            "Signature should be valid"
         );
     }
 }
