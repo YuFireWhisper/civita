8e899179e0dfe2f038300aef42e97b2349e49bd8
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-11 10:52:50 +0800

    feat: Add vrf_vote_duration field to Config of Vrf

--- src/crypto/vrf/config.rs
+++ src/crypto/vrf/config.rs
@@ -1,7 +1,8 @@
 use tokio::time::Duration;
 
 pub const DEFAULT_THRESHOLD_PERCENTAGE: f64 = 0.67;
-const DEFAULT_VRF_WAIT_DURATION: Duration = Duration::from_millis(5000);
+const DEFAULT_VRF_PROOF_DURATION: Duration = Duration::from_millis(5000);
+const DEFAULT_VRF_VOTE_DURATION: Duration = Duration::from_millis(5000);
 const DEFAULT_CHECK_INTERVAL: Duration = Duration::from_millis(1000);
 const DEFAULT_TOPIC: &str = "vrf";
 const DEFAULT_MIN_RESPONSES: usize = 3;
@@ -10,7 +11,8 @@ const DEFAULT_CANDIDATES_COUNT: usize = 10;
 #[derive(Debug, Clone)]
 pub struct Config {
     pub topic: String,
-    pub vrf_wait_duration: Duration,
+    pub vrf_proof_duration: Duration,
+    pub vrf_vote_duration: Duration,
     pub check_interval: Duration,
     pub threshold_percentage: f64,
     pub min_responses: usize,
@@ -21,7 +23,8 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             topic: DEFAULT_TOPIC.to_string(),
-            vrf_wait_duration: DEFAULT_VRF_WAIT_DURATION,
+            vrf_proof_duration: DEFAULT_VRF_PROOF_DURATION,
+            vrf_vote_duration: DEFAULT_VRF_VOTE_DURATION,
             check_interval: DEFAULT_CHECK_INTERVAL,
             threshold_percentage: DEFAULT_THRESHOLD_PERCENTAGE,
             min_responses: DEFAULT_MIN_RESPONSES,
