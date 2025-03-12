252a3a4152444001d5fe6cf1567295b5b994e7a8
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-12 14:32:06 +0800

    feat: Impl add_subscription

--- src/network/transport/libp2p_transport/subscription.rs
+++ src/network/transport/libp2p_transport/subscription.rs
@@ -14,19 +14,48 @@ impl Subscription {
             subscriptions: HashMap::new(),
         }
     }
+
+    pub fn add_subscription(&mut self, filter: SubscriptionFilter, sender: Sender<Message>) {
+        self.subscriptions.entry(filter).or_default().push(sender);
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use tokio::sync::mpsc::channel;
+
     use super::*;
+
+    const TEST_TOPIC: &str = "test";
 
     fn create_subscription() -> Subscription {
         Subscription::new()
+    }
+
+    fn create_filter() -> SubscriptionFilter {
+        SubscriptionFilter::Topic(TEST_TOPIC.to_string())
+    }
+
+    fn create_sender() -> Sender<Message> {
+        let (sender, _) = channel(1);
+        sender
     }
 
     #[test]
     fn test_new() {
         let subscription = create_subscription();
         assert!(subscription.subscriptions.is_empty());
+    }
+
+    #[test]
+    fn test_add_subscription() {
+        let mut subscription = create_subscription();
+        let filter = create_filter();
+        let sender = create_sender();
+
+        subscription.add_subscription(filter.clone(), sender);
+
+        assert_eq!(subscription.subscriptions.len(), 1);
+        assert_eq!(subscription.subscriptions.get(&filter).unwrap().len(), 1);
     }
 }
