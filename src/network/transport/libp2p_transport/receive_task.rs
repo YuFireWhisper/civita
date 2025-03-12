d7e3c09829396266bb7d9ed476edc52b8ff0ac88
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-12 14:18:46 +0800

    feat: Impl stop

--- src/network/transport/libp2p_transport/receive_task.rs
+++ src/network/transport/libp2p_transport/receive_task.rs
@@ -12,6 +12,12 @@ impl<T> ReceiveTask<T> {
     pub fn is_running(&self) -> bool {
         self.handle.is_some() && !self.handle.as_ref().unwrap().is_finished() // unwrap is safe
     }
+
+    pub fn stop(&mut self) {
+        if let Some(handle) = self.handle.take() {
+            handle.abort();
+        }
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +66,20 @@ mod tests {
         task.handle = Some(spawn(async { TEST_VALUE }));
         sleep(TEST_DURATION).await;
         assert!(!task.is_running(), "Task should not be running");
+    }
+
+    #[tokio::test]
+    async fn test_stop() {
+        let mut task = create_task();
+        task.handle = Some(spawn(async {
+            sleep(TEST_DURATION).await;
+            TEST_VALUE
+        }));
+        let is_running_before = task.is_running();
+        task.stop();
+        let is_running_after = task.is_running();
+
+        assert!(is_running_before, "Task should be running before stop");
+        assert!(!is_running_after, "Task should not be running after stop");
     }
 }
