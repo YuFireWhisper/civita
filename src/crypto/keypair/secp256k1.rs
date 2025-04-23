44104328fbbfe5f97918f028d780d102d43d5d6a
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-04-23 20:46:46 +0800

    feat: add VRF capabilities to secp256k1 keypair
    - Implement prove() method for SecretKey to generate VRF proofs
    - Add verify_proof() method for PublicKey to validate VRF proofs
    - Include test case to verify VRF functionality

--- src/crypto/keypair/secp256k1.rs
+++ src/crypto/keypair/secp256k1.rs
@@ -1,6 +1,8 @@
 use bincode::{Decode, Encode};
+use libecvrf::extends::{AffineExtend, ScalarExtend};
 use libsecp256k1::{self, PublicKey as LibPublicKey, PublicKeyFormat, SecretKey as LibSecretKey};
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 
 const SECRET_KEY_LENGTH: usize = 32;
 const PUBLIC_KEY_LENGTH: usize = 33;
@@ -62,6 +64,15 @@ impl SecretKey {
     pub fn decrypt(&self, ciphertext: &[u8]) -> Result<Vec<u8>> {
         ecies::decrypt(&self.0, ciphertext).map_err(Error::from)
     }
+
+    pub fn prove(&self, input: impl AsRef<[u8]>) -> libecvrf::ECVRFProof {
+        let hash = Sha256::digest(input.as_ref()).to_vec();
+        let secret_key = libecvrf::secp256k1::SecretKey::parse(&self.0)
+            .expect("Invalid secret key, it should not happen");
+        let ecvrf = libecvrf::ECVRF::new(secret_key);
+        let input_scalar = libecvrf::secp256k1::curve::Scalar::from_bytes(&hash);
+        ecvrf.prove(&input_scalar)
+    }
 }
 
 impl PublicKey {
@@ -84,6 +95,48 @@ impl PublicKey {
 
     pub fn encrypt(&self, msg: &[u8]) -> Result<Vec<u8>> {
         ecies::encrypt(&self.0, msg).map_err(Error::from)
+    }
+
+    pub fn verify_proof(&self, proof: &libecvrf::ECVRFProof, input: impl AsRef<[u8]>) -> bool {
+        use libecvrf::secp256k1::{
+            curve::{Affine, Jacobian, Scalar, AFFINE_G},
+            PublicKey as LibecvrfPublicKey, ECMULT_CONTEXT,
+        };
+
+        let public_key = LibecvrfPublicKey::parse_compressed(&self.0)
+            .expect("Invalid public key, it should not happen");
+        let mut pub_affine: Affine = public_key.into();
+        pub_affine.x.normalize();
+        pub_affine.y.normalize();
+
+        let input_hash = Sha256::digest(input.as_ref()).to_vec();
+        let input_scalar = Scalar::from_bytes(&input_hash);
+        let h = libecvrf::hash::hash_to_curve(&input_scalar, Some(&pub_affine));
+        let mut jh = Jacobian::default();
+        jh.set_ge(&h);
+
+        let mut u = Jacobian::default();
+        let pub_jacobian = Jacobian::from_ge(&pub_affine);
+
+        ECMULT_CONTEXT.ecmult(&mut u, &pub_jacobian, &proof.c, &proof.s);
+
+        let witness_gamma = libecvrf::helper::ecmult(&ECMULT_CONTEXT, &proof.gamma, &proof.c);
+        let witness_hash = libecvrf::helper::ecmult(&ECMULT_CONTEXT, &h, &proof.s);
+
+        let v = Jacobian::from_ge(&witness_gamma).add_ge(&witness_hash);
+
+        let computed_c = libecvrf::hash::hash_points(
+            &AFFINE_G,
+            &h,
+            &pub_affine,
+            &proof.gamma,
+            &Affine::from_jacobian(&u),
+            &Affine::from_jacobian(&v),
+        );
+
+        let computed_y = Scalar::from_bytes(&proof.gamma.keccak256());
+
+        computed_c == proof.c && computed_y == proof.y
     }
 }
 
@@ -173,5 +226,15 @@ mod tests {
             sk2.decrypt(&ciphertext).is_err(),
             "Decryption should fail with different keys"
         );
+    }
+
+    #[test]
+    fn vrf_success() {
+        const MESSAGE: &[u8] = b"Hello, world!";
+        const MESSAGE_HASH: &[u8] = b"Hello, world!";
+
+        let (sk, pk) = super::generate_keypair();
+        let proof = sk.prove(MESSAGE);
+        assert!(pk.verify_proof(&proof, MESSAGE_HASH));
     }
 }
