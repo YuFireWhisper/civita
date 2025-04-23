2ea89f4eba9d7ee9fb5210f4916081d4e24f0bc5
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-04-23 10:02:32 +0800

    feat: implement common signature interface
    - Create top-level Signature enum to wrap specific implementations
    - Add verify method on the Signature enum
    - Modify TSS trait to use common Signature type
    - Add Serialize/Deserialize support for signatures

--- src/crypto/tss.rs
+++ src/crypto/tss.rs
@@ -1,9 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+use crate::crypto::primitives::algebra::Point;
+
 pub mod schnorr;
+
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+pub enum Signature {
+    Schnorr(schnorr::signature::Signature),
+}
 
 pub trait Tss {
     type Error;
-    type Signature;
 
-    fn sign(&self, seed: Option<&[u8]>, msg: &[u8]) -> Result<Self::Signature, Self::Error>;
-    fn verify(&self, msg: &[u8], sig: &Self::Signature) -> bool;
+    fn sign(&self, seed: Option<&[u8]>, msg: &[u8]) -> Result<Signature, Self::Error>;
+    fn verify(&self, msg: &[u8], sig: &Signature) -> bool;
+}
+
+impl Signature {
+    pub fn verify(&self, msg: &[u8], public_key: &Point) -> bool {
+        match self {
+            Signature::Schnorr(sig) => sig.verify(msg, public_key),
+        }
+    }
 }
