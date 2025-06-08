a8c4175e7096e918775e09a7ed944c3b101a32a7
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-06-08 15:14:35 +0800

    refactor: redesign crypto trait interfaces for signatures and VRF
    - Replace Signature trait with Signer and Verifier
    - Redesign VRF interfaces with Prover and Verifier traits
    - Improve type safety with associated types
    - Make interfaces more consistent across crypto components

--- src/crypto/traits/signature.rs
+++ src/crypto/traits/signature.rs
@@ -1,1 +1,16 @@
-pub trait Signature {}
+use crate::crypto::{
+    self,
+    traits::{public_key::PublicKey, secret_key::SecretKey},
+};
+
+pub trait Signer: SecretKey {
+    type Signature;
+
+    fn sign(&self, msg: &[u8]) -> Result<Self::Signature, crypto::Error>;
+}
+
+pub trait Verifier: PublicKey {
+    type Signature;
+
+    fn verify(&self, msg: &[u8], sig: &Self::Signature) -> Result<bool, crypto::Error>;
+}
--- src/crypto/traits/vrf.rs
+++ src/crypto/traits/vrf.rs
@@ -1,16 +1,16 @@
-use crate::crypto::{self, traits::suite::Suite};
+use crate::crypto::{
+    self,
+    traits::{public_key::PublicKey, secret_key::SecretKey},
+};
 
-pub trait Proof {
-    fn to_output(&self) -> Vec<u8>;
+pub trait Prover: SecretKey {
+    type Proof;
+
+    fn prove(&self, alpha: &[u8]) -> Result<Self::Proof, crypto::Error>;
 }
 
-pub trait Vrf: Suite {
-    type Proof: Proof;
+pub trait Verifier: PublicKey {
+    type Proof;
 
-    fn prove(secret_key: &Self::SecretKey, input: &[u8]) -> Self::Proof;
-    fn verify(
-        public_key: &Self::PublicKey,
-        input: &[u8],
-        proof: &Self::Proof,
-    ) -> Result<Vec<u8>, crypto::Error>;
+    fn verify(&self, alpha: &[u8], pi: Self::Proof) -> Result<Vec<u8>, crypto::Error>;
 }
