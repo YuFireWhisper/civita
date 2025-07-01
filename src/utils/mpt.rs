687eea963d04062a9251b1c82a6a962b789a731e
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-07-01 13:33:04 +0800

    refactor: optimize MPT implementation with CacheStorage
    - Replace custom Staging with more efficient CacheStorage
    - Change Nibble type from u8 to u16 for improved path handling
    - Simplify function names and improve code readability
    - Remove unnecessary cloning and improve memory usage
    - Integrate with new Storage trait implementation

--- src/utils/mpt.rs
+++ src/utils/mpt.rs
@@ -1,46 +1,37 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, sync::Arc};
+
+use bytemuck::cast_slice;
 
 use crate::{
     crypto::{traits::hasher::Multihash, Hasher},
-    network::transport,
-    traits::Serializable,
-    utils::mpt::{
-        node::{Branch, Extension, Leaf, Node},
-        staging::Staging,
+    network::{
+        storage::{CacheStorage, Storage},
+        transport::Kad,
     },
+    traits::Serializable,
+    utils::mpt::node::{Branch, Extension, Leaf, Node},
 };
 
-#[cfg(not(test))]
-use crate::network::transport::Transport;
-
-#[cfg(test)]
-use crate::network::transport::MockTransport as Transport;
-
 mod node;
-mod staging;
 
-type Nibble = u8;
+type Nibble = u16;
 
-#[async_trait::async_trait]
-pub trait Storage<V> {
-    type Error;
-
-    async fn put(&self, key: &Multihash, value: &Node<V>) -> Result<(), Self::Error>;
-    async fn get(&self, key: &Multihash) -> Result<Option<Node<V>>, Self::Error>;
-}
-
-pub struct Mpt<V, S: Storage<V> = Transport> {
+pub struct Mpt<T, S: Storage = Arc<Kad>> {
     root_hash: Option<Multihash>,
     original_root: Option<Multihash>,
-    staging: Staging<V, S>,
+    storage: CacheStorage<Node<T>, S>,
 }
 
-impl<V: Serializable + Clone, S: Storage<V>> Mpt<V, S> {
+impl<T, S> Mpt<T, S>
+where
+    T: Clone + Serializable + Send + Sync + 'static,
+    S: Storage,
+{
     pub fn new(storage: S) -> Self {
         Self {
             root_hash: None,
             original_root: None,
-            staging: Staging::new(storage),
+            storage: CacheStorage::new(storage),
         }
     }
 
@@ -48,79 +39,89 @@ impl<V: Serializable + Clone, S: Storage<V>> Mpt<V, S> {
         Self {
             root_hash: Some(root_hash),
             original_root: Some(root_hash),
-            staging: Staging::new(storage),
+            storage: CacheStorage::new(storage),
         }
     }
 
-    pub async fn insert<H: Hasher>(&mut self, key: &[u8], value: V) -> Result<(), S::Error> {
+    pub async fn insert<H: Hasher>(&mut self, path: &[u8], value: T) -> Result<(), S::Error> {
+        let key = cast_slice(path);
+
         let root = self.get_root_node().await?;
-        let new_root = self.insert_recursive::<H>(root, key, value).await?;
+        let new_root = self.insert_rec::<H>(root, key, value).await?;
 
         let new_root_hash = new_root.hash::<H>();
-        self.staging.put(new_root_hash, new_root);
+        self.storage.insert(new_root_hash, new_root);
 
         self.root_hash = Some(new_root_hash);
 
         Ok(())
     }
 
-    async fn get_root_node(&mut self) -> Result<Node<V>, S::Error> {
-        match &self.root_hash {
-            Some(hash) => Ok(self.staging.get(hash).await?.unwrap_or(Node::Empty)),
+    async fn get_root_node(&mut self) -> Result<Node<T>, S::Error> {
+        match self.root_hash {
+            Some(hash) => self.get_node(&hash).await,
             None => Ok(Node::Empty),
         }
     }
 
-    async fn insert_recursive<H: Hasher>(
+    async fn get_node(&mut self, hash: &Multihash) -> Result<Node<T>, S::Error> {
+        self.storage
+            .get(hash)
+            .await?
+            .map_or_else(|| Ok(Node::Empty), |node| Ok(node.clone()))
+    }
+
+    async fn insert_rec<H: Hasher>(
         &mut self,
-        node: Node<V>,
-        key: &[Nibble],
-        value: V,
-    ) -> Result<Node<V>, S::Error> {
+        node: Node<T>,
+        path: &[Nibble],
+        value: T,
+    ) -> Result<Node<T>, S::Error> {
         match node {
-            Node::Empty => Ok(self.create_leaf_node(key, value)),
-            Node::Leaf(leaf) => Box::pin(self.insert_leaf::<H>(leaf, key, value)).await,
-            Node::Extension(ext) => Box::pin(self.insert_extension::<H>(ext, key, value)).await,
-            Node::Branch(branch) => Box::pin(self.insert_branch::<H>(branch, key, value)).await,
+            Node::Empty => Ok(self.create_leaf(path, value)),
+            Node::Leaf(leaf) => Box::pin(self.insert_leaf::<H>(leaf, path, value)).await,
+            Node::Extension(ext) => Box::pin(self.insert_ext::<H>(ext, path, value)).await,
+            Node::Branch(branch) => Box::pin(self.insert_branch::<H>(branch, path, value)).await,
         }
     }
 
-    fn create_leaf_node(&self, key: &[Nibble], value: V) -> Node<V> {
+    fn create_leaf(&self, path: &[Nibble], value: T) -> Node<T> {
         Node::Leaf(Leaf {
-            path: key.to_vec(),
+            path: path.to_vec(),
             value,
         })
     }
 
     async fn insert_leaf<H: Hasher>(
         &mut self,
-        leaf: Leaf<V>,
-        key: &[Nibble],
-        value: V,
-    ) -> Result<Node<V>, S::Error> {
-        let common = Self::common_prefix(&leaf.path, key);
+        leaf: Leaf<T>,
+        path: &[Nibble],
+        value: T,
+    ) -> Result<Node<T>, S::Error> {
+        let common = Self::common_prefix(&leaf.path, path);
 
-        if common.len() == leaf.path.len() && common.len() == key.len() {
-            return Ok(Node::new_leaf(leaf.path.clone(), value));
+        if common.len() == leaf.path.len() && common.len() == path.len() {
+            return Ok(Node::new_leaf(leaf.path, value));
         }
 
         if common.len() == leaf.path.len() {
-            let remaining = &key[common.len()..];
+            let path = &path[common.len()..];
 
-            let child = self
-                .insert_recursive::<H>(Node::Empty, &remaining[1..], value)
-                .await?;
-            let child_hash = child.hash::<H>();
+            let child = self.insert_rec::<H>(Node::Empty, &path[1..], value).await?;
+            let hash = self.hash_and_insert::<H>(child)?;
 
-            self.staging.put(child_hash, child);
+            let children = BTreeMap::from([(path[0], hash)]);
 
-            let children = BTreeMap::from([(remaining[0], child_hash)]);
-
-            return Ok(Node::new_branch(children, Some(leaf.value.clone())));
+            return Ok(Node::new_branch(children, Some(leaf.value)));
         }
 
-        self.split_leaf::<H>(&leaf.path, leaf.value.clone(), key, value, common.len())
-            .await
+        self.split_leaf::<H>(leaf, path, value, common.len()).await
+    }
+
+    fn hash_and_insert<H: Hasher>(&mut self, node: Node<T>) -> Result<Multihash, S::Error> {
+        let hash = node.hash::<H>();
+        self.storage.insert(hash, node);
+        Ok(hash)
     }
 
     fn common_prefix(a: &[Nibble], b: &[Nibble]) -> Vec<Nibble> {
@@ -137,38 +138,27 @@ impl<V: Serializable + Clone, S: Storage<V>> Mpt<V, S> {
 
     async fn split_leaf<H: Hasher>(
         &mut self,
-        leaf_key: &[Nibble],
-        leaf_value: V,
-        new_key: &[Nibble],
-        new_value: V,
+        leaf: Leaf<T>,
+        path: &[Nibble],
+        value: T,
         common_len: usize,
-    ) -> Result<Node<V>, S::Error> {
-        let remaining_leaf_key = &leaf_key[common_len..];
-        let remaining_new_key = &new_key[common_len..];
+    ) -> Result<Node<T>, S::Error> {
+        let leaf_path = &leaf.path[common_len..];
+        let new_path = &path[common_len..];
 
         let mut children = BTreeMap::new();
 
-        let leaf_child = if remaining_leaf_key.len() == 1 {
-            Node::new_leaf(vec![], leaf_value)
-        } else {
-            Node::new_leaf(remaining_leaf_key[1..].to_vec(), leaf_value)
-        };
+        {
+            let child = Self::new_leaf_child(&leaf_path, leaf.value);
+            let hash = self.hash_and_insert::<H>(child)?;
+            children.insert(leaf_path[0], hash);
+        }
 
-        let leaf_child_hash = leaf_child.hash::<H>();
-        self.staging.put(leaf_child_hash, leaf_child);
-
-        children.insert(remaining_leaf_key[0], leaf_child_hash);
-
-        let new_child = if remaining_new_key.len() == 1 {
-            Node::new_leaf(vec![], new_value)
-        } else {
-            Node::new_leaf(remaining_new_key[1..].to_vec(), new_value)
-        };
-
-        let new_child_hash = new_child.hash::<H>();
-        self.staging.put(new_child_hash, new_child);
-
-        children.insert(remaining_new_key[0], new_child_hash);
+        {
+            let child = Self::new_leaf_child(&new_path, value);
+            let hash = self.hash_and_insert::<H>(child)?;
+            children.insert(new_path[0], hash);
+        }
 
         let branch = Node::new_branch(children, None);
 
@@ -176,71 +166,70 @@ impl<V: Serializable + Clone, S: Storage<V>> Mpt<V, S> {
             return Ok(branch);
         }
 
-        let branch_hash = branch.hash::<H>();
-        self.staging.put(branch_hash, branch);
+        let hash = self.hash_and_insert::<H>(branch)?;
 
-        Ok(Node::new_extension(
-            leaf_key[..common_len].to_vec(),
-            branch_hash,
-        ))
+        Ok(Node::new_extension(leaf_path[..common_len].to_vec(), hash))
     }
 
-    async fn insert_extension<H: Hasher>(
+    fn new_leaf_child(path: &[Nibble], value: T) -> Node<T> {
+        if path.len() == 1 {
+            Node::new_leaf(vec![], value)
+        } else {
+            Node::new_leaf(path[1..].to_vec(), value)
+        }
+    }
+
+    async fn insert_ext<H: Hasher>(
         &mut self,
         ext: Extension,
-        key: &[Nibble],
-        value: V,
-    ) -> Result<Node<V>, S::Error> {
-        let common = Self::common_prefix(&ext.path, key);
+        path: &[Nibble],
+        value: T,
+    ) -> Result<Node<T>, S::Error> {
+        let common = Self::common_prefix(&ext.path, path);
 
         if common.len() == ext.path.len() {
-            let remaining = &key[common.len()..];
+            let path = &path[common.len()..];
 
-            let child = self.staging.get(&ext.child).await?.unwrap_or(Node::Empty);
-            let new_child = self.insert_recursive::<H>(child, remaining, value).await?;
+            let child = self.get_node(&ext.child).await?;
+            let child = self.insert_rec::<H>(child, path, value).await?;
+            let hash = self.hash_and_insert::<H>(child)?;
 
-            let new_child_hash = new_child.hash::<H>();
-            self.staging.put(new_child_hash, new_child);
-
-            Ok(Node::new_extension(ext.path.clone(), new_child_hash))
+            Ok(Node::new_extension(ext.path, hash))
         } else {
-            self.split_extension::<H>(&ext.path, ext.child, key, value, common.len())
-                .await
+            self.split_ext::<H>(&ext, path, value, common.len()).await
         }
     }
 
-    async fn split_extension<H: Hasher>(
+    async fn split_ext<H: Hasher>(
         &mut self,
-        ext_key: &[Nibble],
-        ext_next: Multihash,
-        new_key: &[Nibble],
-        new_value: V,
+        ext: &Extension,
+        path: &[Nibble],
+        value: T,
         common_len: usize,
-    ) -> Result<Node<V>, S::Error> {
-        let ext_remaining = &ext_key[common_len..];
-        let new_remaining = &new_key[common_len..];
+    ) -> Result<Node<T>, S::Error> {
+        let ext_path = &ext.path[common_len..];
+        let new_path = &path[common_len..];
 
         let mut children = BTreeMap::new();
 
-        let ext_child = if ext_remaining.len() == 1 {
-            ext_next
-        } else {
-            let new_ext = Node::new_extension(ext_remaining[1..].to_vec(), ext_next);
-            let ext_child_hash = new_ext.hash::<H>();
-            self.staging.put(ext_child_hash, new_ext);
-            ext_child_hash
-        };
+        {
+            let child = if ext_path.len() == 1 {
+                ext.child
+            } else {
+                let ext = Node::new_extension(ext_path[1..].to_vec(), ext.child);
+                self.hash_and_insert::<H>(ext)?
+            };
 
-        children.insert(ext_remaining[0], ext_child);
+            children.insert(ext_path[0], child);
+        }
 
-        let new_child = self
-            .insert_recursive::<H>(Node::Empty, &new_remaining[1..], new_value)
-            .await?;
-
-        let new_child_hash = new_child.hash::<H>();
-        self.staging.put(new_child_hash, new_child);
-
-        children.insert(new_remaining[0], new_child_hash);
+        {
+            let child = self
+                .insert_rec::<H>(Node::Empty, &new_path[1..], value)
+                .await?;
+            let hash = self.hash_and_insert::<H>(child)?;
+            children.insert(new_path[0], hash);
+        }
 
         let branch = Node::new_branch(children, None);
 
@@ -248,120 +237,95 @@ impl<V: Serializable + Clone, S: Storage<V>> Mpt<V, S> {
             return Ok(branch);
         }
 
-        let branch_hash = branch.hash::<H>();
-        self.staging.put(branch_hash, branch);
+        let hash = self.hash_and_insert::<H>(branch)?;
 
-        Ok(Node::new_extension(
-            ext_key[..common_len].to_vec(),
-            branch_hash,
-        ))
+        Ok(Node::new_extension(ext_path[..common_len].to_vec(), hash))
     }
 
     async fn insert_branch<H: Hasher>(
         &mut self,
-        mut branch: Branch<V>,
+        mut branch: Branch<T>,
         key: &[Nibble],
-        value: V,
-    ) -> Result<Node<V>, S::Error> {
+        value: T,
+    ) -> Result<Node<T>, S::Error> {
         if key.is_empty() {
             branch.value = Some(value);
         } else {
             let idx = key[0];
-
             let child = match branch.children.get(&idx) {
-                Some(child_hash) => self.staging.get(child_hash).await?.unwrap_or(Node::Empty),
+                Some(hash) => self.get_node(hash).await?,
                 None => Node::Empty,
             };
-            let new_child = self.insert_recursive::<H>(child, &key[1..], value).await?;
-
-            let new_child_hash = new_child.hash::<H>();
-            self.staging.put(new_child_hash, new_child);
-
-            branch.children.insert(idx, new_child_hash);
+            let child = self.insert_rec::<H>(child, &key[1..], value).await?;
+            let hash = self.hash_and_insert::<H>(child)?;
+            branch.children.insert(idx, hash);
         }
 
-        Ok(Node::new_branch(branch.children, branch.value.clone()))
+        Ok(Node::Branch(branch))
     }
 
-    pub async fn get(&mut self, key: &[Nibble]) -> Result<Option<V>, S::Error> {
-        if let Some(root_hash) = &self.root_hash {
-            let root_node = self.staging.get(root_hash).await?.unwrap_or(Node::Empty);
-            self.get_node(&root_node, key).await
-        } else {
-            Ok(None)
-        }
+    pub async fn get(&mut self, key: &[u8]) -> Result<Option<T>, S::Error> {
+        let key = cast_slice(key);
+
+        let Some(hash) = self.root_hash else {
+            return Ok(None);
+        };
+
+        let root = self.get_node(&hash).await?;
+        self.get_rec(&root, key).await
     }
 
-    async fn get_node(&mut self, node: &Node<V>, key: &[Nibble]) -> Result<Option<V>, S::Error> {
+    async fn get_rec(&mut self, node: &Node<T>, path: &[Nibble]) -> Result<Option<T>, S::Error> {
         match node {
             Node::Empty => Ok(None),
             Node::Leaf(leaf) => {
-                if leaf.path == key {
+                if leaf.path == path {
                     Ok(Some(leaf.value.clone()))
                 } else {
                     Ok(None)
                 }
             }
             Node::Extension(ext) => {
-                if key.starts_with(&ext.path) {
-                    let child = self.staging.get(&ext.child).await?.unwrap_or(Node::Empty);
-                    return Box::pin(self.get_node(&child, &key[ext.path.len()..])).await;
-                }
-
-                return Ok(None);
-            }
-            Node::Branch(branch) => {
-                if key.is_empty() {
-                    return Ok(branch.value.clone());
-                }
-
-                let idx = key[0];
-
-                if let Some(child_hash) = branch.children.get(&idx) {
-                    let child = self.staging.get(child_hash).await?.unwrap_or(Node::Empty);
-                    return Box::pin(self.get_node(&child, &key[1..])).await;
+                if path.starts_with(&ext.path) {
+                    let path = &path[ext.path.len()..];
+                    let child = self.get_node(&ext.child).await?;
+                    return Box::pin(self.get_rec(&child, path)).await;
                 }
 
                 Ok(None)
+            }
+            Node::Branch(branch) => {
+                if path.is_empty() {
+                    return Ok(branch.value.clone());
+                }
+
+                let idx = path[0];
+
+                match branch.children.get(&idx) {
+                    Some(hash) => {
+                        let path = &path[1..];
+                        let child = self.get_node(hash).await?;
+                        return Box::pin(self.get_rec(&child, path)).await;
+                    }
+                    None => Ok(None),
+                }
             }
         }
     }
 
     pub async fn commit(&mut self) -> Result<(), S::Error> {
-        self.staging.commit().await?;
-        self.original_root = self.root_hash.clone();
+        self.storage.commit().await?;
+        self.original_root = self.root_hash;
 
         Ok(())
     }
 
     pub fn rollback(&mut self) {
-        self.staging.rollback();
-        self.root_hash = self.original_root.clone();
-    }
-
-    pub fn has_uncommitted_changes(&self) -> bool {
-        self.staging.has_uncommitted_changes()
-    }
-
-    pub fn uncommitted_count(&self) -> usize {
-        self.staging.uncommitted_count()
+        self.storage.rollback();
+        self.root_hash = self.original_root;
     }
 
     pub fn root_hash(&self) -> Option<Multihash> {
         self.root_hash
-    }
-}
-
-#[async_trait::async_trait]
-impl<V: Serializable + Clone + Sync + 'static> Storage<V> for Transport {
-    type Error = transport::Error;
-
-    async fn put(&self, key: &Multihash, value: &Node<V>) -> Result<(), Self::Error> {
-        self.put_with_key(key, value.to_vec().expect("Failed to serialize Node"))
-            .await
-    }
-
-    async fn get(&self, key: &Multihash) -> Result<Option<Node<V>>, Self::Error> {
-        self.get::<Node<V>>(key).await
     }
 }
