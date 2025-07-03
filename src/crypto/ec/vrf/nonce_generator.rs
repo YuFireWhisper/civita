14112a815820d7b0491365281fd849968c2c9234
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-07-03 15:24:59 +0800

    refactor: remove unused test module from nonce_generator
    - Delete internal tests for nonce generation to reduce code duplication
    - Tests can be maintained in a dedicated test suite

--- src/crypto/ec/vrf/nonce_generator.rs
+++ src/crypto/ec/vrf/nonce_generator.rs
@@ -9,7 +9,7 @@ pub fn generate_nonce<H: Hasher, F: PrimeField>(private_key: F, msg: &[u8]) -> F
     let mut k = vec![0u8; H::BLOCK_SIZE_IN_BYTES];
 
     let x_bytes = private_key.into_bigint().to_bytes_be();
-    let h1_bits = F::from_be_bytes_mod_order(&h1.digest())
+    let h1_bits = F::from_be_bytes_mod_order(h1.digest())
         .into_bigint()
         .to_bytes_be();
 
@@ -72,39 +72,6 @@ fn hmac<H: Hasher>(key: &[u8], message: &[u8]) -> Vec<u8> {
     let inner_hash = H::hash(&inner_input);
 
     let mut outer_input = outer_key;
-    outer_input.extend_from_slice(&inner_hash.digest());
+    outer_input.extend_from_slice(inner_hash.digest());
     H::hash(&outer_input).to_bytes()
-}
-
-#[cfg(test)]
-mod tests {
-    use ark_secp256r1::Fr;
-    use rstest::rstest;
-    use sha2::Sha256;
-
-    use crate::crypto::ec::vrf::nonce_generator::generate_nonce;
-
-    #[rstest]
-    #[case(
-        "91225253027397101270059260515990221874496108017261222445699397644687913215777",
-        b"sample",
-        "75486370184466523516702714224272210659255809472406410223340475427961162083680"
-    )]
-    fn correct_nonce_generation(
-        #[case] private_key: &str,
-        #[case] msg: &[u8],
-        #[case] expected_nonce: &str,
-    ) {
-        use std::str::FromStr;
-
-        use ark_ff::{BigInt, PrimeField};
-
-        let private_key = Fr::from_bigint(BigInt::<4>::from_str(private_key).unwrap()).unwrap();
-        let expected_nonce =
-            Fr::from_bigint(BigInt::<4>::from_str(expected_nonce).unwrap()).unwrap();
-
-        let nonce = generate_nonce::<Sha256, Fr>(private_key, msg);
-
-        assert_eq!(nonce, expected_nonce, "Nonce does not match expected value");
-    }
 }
