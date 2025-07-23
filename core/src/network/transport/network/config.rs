91f5919c19df346177960663c66d79a04b593501
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-07-23 08:45:07 +0800

    Cleanup unused Network::Config field

--- core/src/network/transport/network/config.rs
+++ core/src/network/transport/network/config.rs
@@ -1,9 +1,7 @@
 use tokio::time::Duration;
 
 const DEFAULT_CHECK_LISTEN_TIMEOUT: Duration = Duration::from_millis(100);
-const DEFAULT_CHECK_DIAL_TIMEOUT: Duration = Duration::from_secs(5);
 const DEFAULT_CHANNEL_SIZE: usize = 1000;
-const DEFAULT_CLEANUP_CHANNEL_INTERVAL: Duration = Duration::from_secs(60);
 const DEFAULT_GET_SWARM_LOCK_TIMEOUT: Duration = Duration::from_secs(5);
 const DEFAULT_WAIT_FOR_GOSSIPSUB_PEER_TIMEOUT: Duration = Duration::from_secs(10);
 const DEFAULT_WAIT_FOR_GOSSIPSUB_PEER_INTERVAL: Duration = Duration::from_millis(100);
@@ -14,9 +12,7 @@ const DEFAULT_RECEIVE_INTERVAL: Duration = Duration::from_millis(100);
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Config {
     pub check_listen_timeout: Duration,
-    pub check_dial_timeout: Duration,
     pub channel_capacity: usize,
-    pub cleanup_channel_interval: Duration,
     pub get_swarm_lock_timeout: Duration,
     pub wait_for_gossipsub_peer_timeout: Duration,
     pub wait_for_gossipsub_peer_interval: Duration,
@@ -28,9 +24,7 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         let check_listen_timeout = DEFAULT_CHECK_LISTEN_TIMEOUT;
-        let check_dial_timeout = DEFAULT_CHECK_DIAL_TIMEOUT;
         let channel_capacity = DEFAULT_CHANNEL_SIZE;
-        let cleanup_channel_interval = DEFAULT_CLEANUP_CHANNEL_INTERVAL;
         let get_swarm_lock_timeout = DEFAULT_GET_SWARM_LOCK_TIMEOUT;
         let wait_for_gossipsub_peer_timeout = DEFAULT_WAIT_FOR_GOSSIPSUB_PEER_TIMEOUT;
         let wait_for_gossipsub_peer_interval = DEFAULT_WAIT_FOR_GOSSIPSUB_PEER_INTERVAL;
@@ -40,9 +34,7 @@ impl Default for Config {
 
         Self {
             check_listen_timeout,
-            check_dial_timeout,
             channel_capacity,
-            cleanup_channel_interval,
             get_swarm_lock_timeout,
             wait_for_gossipsub_peer_timeout,
             wait_for_gossipsub_peer_interval,
