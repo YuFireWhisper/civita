d1b2511e456b97b3a9ebaef0db8008491de7e7c5
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-08-25 14:33:09 +0800

    Define Validator
    Introduce Validator with two functions:
    - validate_script_sig: checks whether script_pk and script_sig match
    - validate_conversion: checks that the input-to-output conversion is correct

---
+++ core/src/consensus/validator.rs
@@ -0,0 +1,11 @@
+use crate::{crypto::Multihash, ty::token::Token};
+
+pub trait Validator {
+    fn validate_script_sig(script_pk: &[u8], script_sig: &[u8]) -> bool;
+    fn validate_conversion<'a>(
+        code: u8,
+        input: impl Iterator<Item = &'a Token>,
+        consumed: impl Iterator<Item = &'a Multihash>,
+        created: &[Token],
+    ) -> bool;
+}
