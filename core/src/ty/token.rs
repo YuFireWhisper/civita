2b27f3116816cdb169a6988c4a3dc4ac0095a080
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-09-02 11:11:14 +0800

    Add id field to Token
    Since the system needs to support multiple hash functions, passing only values and hashing locally would result in inconsistent hashes across different devices. The id field is set by the creator, and all nodes use this field to ensure consistency.

--- core/src/ty/token.rs
+++ core/src/ty/token.rs
@@ -1,32 +1,27 @@
-use std::sync::OnceLock;
-
-use civita_serialize::Serialize;
 use civita_serialize_derive::Serialize;
 
-use crate::crypto::{hasher::Hasher, Multihash};
+use crate::crypto::Multihash;
 
 #[derive(Clone)]
 #[derive(Serialize)]
 pub struct Token {
+    pub id: Multihash,
     pub value: Vec<u8>,
     pub script_pk: Vec<u8>,
-
-    #[serialize(skip)]
-    hash_cache: OnceLock<Multihash>,
 }
 
-impl Token {
-    pub fn new(value: Vec<u8>, script_pk: Vec<u8>) -> Self {
-        Self {
-            value,
-            script_pk,
-            hash_cache: OnceLock::new(),
-        }
-    }
-
-    pub fn hash(&self) -> Multihash {
-        *self
-            .hash_cache
-            .get_or_init(|| Hasher::digest(&self.to_vec()))
-    }
-}
+// impl Token {
+//     pub fn new(value: Vec<u8>, script_pk: Vec<u8>) -> Self {
+//         Self {
+//             value,
+//             script_pk,
+//             hash_cache: OnceLock::new(),
+//         }
+//     }
+//
+//     pub fn hash(&self) -> Multihash {
+//         *self
+//             .hash_cache
+//             .get_or_init(|| Hasher::digest(&self.to_vec()))
+//     }
+// }
