c74c464a54b4bac6ad5d4fca6b25272f29585778
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-09-28 16:06:03 +0800

    Modify Validator Define

--- core/src/consensus/validator.rs
+++ core/src/consensus/validator.rs
@@ -1,9 +1,9 @@
 use libp2p::PeerId;
 
-use crate::ty::{atom::Atom, token::Token};
+use crate::{crypto::Hasher, ty::token::Token};
 
 pub trait Validator: Send + Sync + 'static {
-    fn genesis() -> Atom;
+    fn genesis() -> (Hasher, u8, Vec<Token>);
     fn validate_script_sig(script_pk: &[u8], script_sig: &[u8]) -> bool;
     fn validate_conversion(code: u8, input: &[Token], created: &[Token]) -> bool;
     fn is_related(script_pk: &[u8], peer_id: &PeerId) -> bool;
