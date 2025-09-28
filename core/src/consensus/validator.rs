e33a83f93552f8e54f61ee295ca668ea46e5d592
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-09-28 08:33:31 +0800

    Remove self arg

--- core/src/consensus/validator.rs
+++ core/src/consensus/validator.rs
@@ -3,7 +3,7 @@ use libp2p::PeerId;
 use crate::ty::{atom::Atom, token::Token};
 
 pub trait Validator: Send + Sync + 'static {
-    fn genesis(&self) -> Atom;
+    fn genesis() -> Atom;
     fn validate_script_sig(script_pk: &[u8], script_sig: &[u8]) -> bool;
     fn validate_conversion(code: u8, input: &[Token], created: &[Token]) -> bool;
     fn is_related(script_pk: &[u8], peer_id: &PeerId) -> bool;
