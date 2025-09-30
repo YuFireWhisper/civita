d3785466eb37887905f4c2e718781bc86aa8a858
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-09-30 12:51:51 +0800

    Define Config

---
+++ core/src/config.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+use crate::crypto::Hasher;
+
+type ValuePk<T> = (<T as Config>::Value, <T as Config>::ScriptPk);
+type Diff<T> = Vec<(<T as Config>::Address, Option<ValuePk<T>>)>; // None means deletion
+
+pub trait Executor<T: Config> {
+    fn execute(&self, pk: &T::ScriptPk, sig: &T::ScriptSig) -> Result<Diff<T>, String>;
+}
+
+pub trait Config: Sized + Send + Sync + 'static {
+    type Address: Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+    type Value: Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+    type ScriptPk: Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+    type ScriptSig: Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+    type Event: Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+    type Executor: Executor<Self> + Send + Sync + 'static;
+    type GenesisConfig: GenesisConfig<Self>;
+
+    const HASHER: Hasher;
+}
+
+pub trait GenesisConfig<T: Config>: Send + Sync + 'static {
+    const HEIGHT: u32;
+    const VDF_PARAM: u16;
+    const VAF_DIFFICULTY: u64;
+
+    fn initial_state() -> Vec<(T::Address, ValuePk<T>)>;
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod config;
 pub mod consensus;
 pub mod crypto;
 pub mod network;
