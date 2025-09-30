181d40760690cdec76238b72dfcaea7cc3daf272
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-09-30 13:02:56 +0800

    Update Token define with Config

--- core/src/ty/token.rs
+++ core/src/ty/token.rs
@@ -1,46 +1,14 @@
-use multihash_derive::MultihashDigest;
-
-use crate::crypto::{hasher::Hasher, Multihash};
+use crate::config::Config;
 
 #[derive(Clone)]
 #[derive(serde::Serialize, serde::Deserialize)]
-pub struct Token {
-    pub id: Multihash,
-    pub value: Vec<u8>,
-    pub script_pk: Vec<u8>,
+pub struct Token<T: Config> {
+    pub value: T::Value,
+    pub script_pk: T::ScriptPk,
 }
 
-impl Token {
-    pub fn new(
-        first_input_id: &Multihash,
-        idx: u32,
-        value: impl Into<Vec<u8>>,
-        script_pk: impl Into<Vec<u8>>,
-    ) -> Self {
-        let value = value.into();
-        let script_pk = script_pk.into();
-
-        let mut buf = Vec::new();
-        buf.extend(first_input_id.to_bytes());
-        buf.extend(&idx.to_le_bytes());
-        buf.extend_from_slice(&value);
-        buf.extend_from_slice(&script_pk);
-        let id = Hasher::default().digest(&buf);
-
-        Self {
-            id,
-            value,
-            script_pk,
-        }
-    }
-
-    pub fn validate_id(&self, first_input_id: &Multihash, idx: u32) -> bool {
-        let mut buf = Vec::new();
-        buf.extend(first_input_id.to_bytes());
-        buf.extend(&idx.to_le_bytes());
-        buf.extend_from_slice(&self.value);
-        buf.extend_from_slice(&self.script_pk);
-        let id = Hasher::default().digest(&buf);
-        id == self.id
+impl<T: Config> Token<T> {
+    pub fn new(value: T::Value, script_pk: T::ScriptPk) -> Self {
+        Self { value, script_pk }
     }
 }
