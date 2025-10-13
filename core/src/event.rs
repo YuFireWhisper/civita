56fe0513e72dffbb558d5596fd1acb7e346eeed4
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-10-13 09:11:56 +0800

    Add Proposal

--- core/src/event.rs
+++ core/src/event.rs
@@ -11,13 +11,18 @@ use crate::{
     ty::{Atom, Token},
 };
 
-type OnChainInputs<T> = Vec<(Multihash, <T as traits::Config>::ScriptSig)>;
+pub struct Proposal<T: traits::Config> {
+    pub code: u8,
+    pub on_chain_inputs: Vec<(Multihash, T::ScriptSig)>,
+    pub off_chain_inputs: Vec<T::OffChainInput>,
+    pub outputs: Vec<Token<T>>,
+}
 
 pub enum Event<T: traits::Config> {
     Gossipsub(MessageId, PeerId, Box<Atom<T>>),
     Request(Request, PeerId, ResponseChannel<Response<T>>),
     Response(Response<T>, PeerId),
-    Propose(u8, OnChainInputs<T>, Vec<T::OffChainInput>, Vec<Token<T>>),
+    Propose(Proposal<T>),
     Tokens(oneshot::Sender<HashMap<Multihash, Token<T>>>),
     Status(oneshot::Sender<Status>),
     Stop(oneshot::Sender<()>),
