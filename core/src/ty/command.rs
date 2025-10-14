5964abbdd949761cb55bae266eb31cbe1b5b726b
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-10-14 10:32:00 +0800

    Redefine Command

--- core/src/ty/command.rs
+++ core/src/ty/command.rs
@@ -1,8 +1,6 @@
-use derivative::Derivative;
-use multihash_derive::MultihashDigest;
 use serde::{Deserialize, Serialize};
 
-use crate::{crypto::Multihash, traits::Config, ty::token::Token, utils::mmr::MmrProof};
+use crate::{ty::token::Token, utils::mmr::MmrProof};
 
 #[derive(Clone)]
 #[derive(Debug)]
@@ -13,31 +11,17 @@ pub struct Input {
     pub sig: Vec<u8>,
 }
 
-#[derive(Derivative)]
-#[derivative(Clone(bound = "T: Config"))]
-#[derive(serde::Serialize, serde::Deserialize)]
-#[serde(bound(serialize = "T: Config", deserialize = "T: Config"))]
-pub struct Command<T: Config> {
+#[derive(Clone)]
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+pub struct Command {
     pub code: u8,
-    pub inputs: Vec<Input<T>>,
-    pub outputs: Vec<Token<T>>,
+    pub inputs: Vec<Input>,
+    pub outputs: Vec<Token>,
 }
 
-impl<T: Config> Input<T> {
-    pub fn id(&self) -> Multihash {
-        use bincode::{config, serde::encode_to_vec};
-
-        match self {
-            Input::OnChain(_, id, _, _) => *id,
-            Input::OffChain(_) => {
-                T::HASHER.digest(&encode_to_vec(self, config::standard()).unwrap())
-            }
-        }
-    }
-}
-
-impl<T: Config> Command<T> {
-    pub fn new(code: u8, inputs: Vec<Input<T>>, outputs: Vec<Token<T>>) -> Self {
+impl Command {
+    pub fn new(code: u8, inputs: Vec<Input>, outputs: Vec<Token>) -> Self {
         Self {
             code,
             inputs,
