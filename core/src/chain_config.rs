0920a314cf63b635c51a0dadc6e04d227deb7eb3
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-10-14 12:40:21 +0800

    Define ChainConfig

---
+++ core/src/chain_config.rs
@@ -0,0 +1,16 @@
+use serde::{Deserialize, Serialize};
+
+use crate::crypto::Hasher;
+
+#[derive(Clone, Copy)]
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+pub struct ChainConfig {
+    pub hasher: Hasher,
+    pub vdf_param: u16,
+    pub block_threshold: u32,
+    pub confirmation_depth: u32,
+    pub maintenance_window: u32,
+    pub target_block_time_sec: u64,
+    pub max_vdf_difficulty_adjustment: u32,
+}
